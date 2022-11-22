18b33dd9c3471b39856727ebfa557f12ea736eff
Marcelo Garcia <marcelomgarcia@gmail.com>
2022-11-22 13:13:03 +0300

    scan data_dir

--- src/repo2preservica.py
+++ src/repo2preservica.py
@@ -33,45 +33,30 @@ def main(input):
         LOG.warning("No parent folder was declared in config file.")
         parent = None
 
-    sub_dirs = R2P.get_subdirs(folder.data_folder)
+    bagit_dirs = R2P.get_subdirs(folder.data_folder)
 
     num_submissions = 0
 
-    for dir in sub_dirs:
-        # Using the string representation of 'dir' because pyPreservica can't
-        # handle Pathlib objects yet.
-        str_dir = str(dir)
-        LOG.info(f"Scanning folder '{str_dir}' for ingestion")
-        entities = entity.identifier("code", str_dir)
-        LOG.info(f"Checking number of entries: {len(entities)}")
-        if len(entities) == 0:
-            LOG.info(f"Creating folder '{str_dir}' in Preservica entity")
-            folder_preservica = entity.create_folder(str_dir, str_dir, 
-                folder.security_tag, parent.reference)
-            entity.add_identifier(folder_preservica, "code", str_dir)
-            dir_l1_ref = folder_preservica.reference
-        else:
-            folder_preservica = entities.pop()
-            dir_l1_ref = folder_preservica.reference
 
-        bagit_folders = R2P.get_subdirs(dir)
+    for bag_dir in bagit_dirs:
 
-        # SAve current directory to return after bagging the files.
-        old_dir = OS.getcwd()
+        # To avoid problems we will work with the string 
+        # representation of pathlib object 
+        str_bag = str(bag_dir)
 
-        for bag_dir in bagit_folders:
-            # To avoid problems we will work with the string 
-            # representation of pathlib object 
-            str_bag = str(bag_dir)
+        print(f"bag_dir: {bag_dir}")
+        # Change to directory with data for bagging them
+        #OS.chdir(str_bag)
 
-            # Change to directory with data for bagging them
-            OS.chdir(str_bag)
+
+
+
 
 
 
         #
         #Return the old dir
-        OS.chdir(old_dir)
+        #OS.chdir(old_dir)
 
 
 if __name__ == "__main__":
