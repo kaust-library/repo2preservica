0170029fc8a65e98ca736f04e5d16fc497402bbe
Marcelo Garcia <marcelomgarcia@gmail.com>
2022-12-04 21:29:37 +0300

    Preservica folder and upload

--- src/repo2preservica.py
+++ src/repo2preservica.py
@@ -10,11 +10,6 @@ import os as OS
 import pathlib as PL
 import zipfile as ZIP
 
-metadata_text = """<DeliverableUnit xmlns="http://www.tessella.com/XIP/v4">
-<Title>CATREF</Title>
-<ScopeAndContent>SCOPE</ScopeAndContent>
-</DeliverableUnit>"""
-
 @CL.command()
 @CL.argument('input', type=CL.File('r'))
 def main(input):
@@ -48,6 +43,18 @@ def main(input):
 
     num_submissions = 0
 
+    LOG.info(f"Checking if '{folder.data_folder}' exists in Preservica")
+    entities = entity.identifier("code", folder.data_folder)
+    if len(entities) == 0:
+        data_dir_preservica = entity.create_folder(folder.data_folder, 
+            folder.data_folder, folder.security_tag, folder.parent_folder_id)
+        entity.add_identifier(data_dir_preservica, "code", folder.data_folder)
+        data_dir_reference = data_dir_preservica.reference
+    else:
+
+        data_dir_preservica = entities.pop()
+        data_dir_reference = data_dir_preservica.reference        
+
     LOG.info(f"Start of bagging process.")
     LOG.info(f"We found {len(bagit_dirs)} {len(bagit_dirs) > 1 and 'folders' or 'folder'} to scan")
     for bag_dir in bagit_dirs:
@@ -66,6 +73,10 @@ def main(input):
         result = entity.identifier("code", str_bag)
         LOG.info(f"Results: '{len(result)}'")
         if len(result) == 0:
+            metadata_text = """<DeliverableUnit xmlns="http://www.tessella.com/XIP/v4">
+<Title>CATREF</Title>
+<ScopeAndContent>SCOPE</ScopeAndContent>
+</DeliverableUnit>"""
             metadata_text = metadata_text.replace("CATREF", title)
             metadata_text = metadata_text.replace("SCOPE", description)
             metadata_file = f"{bag_dir}.metadata"
@@ -88,12 +99,15 @@ def main(input):
                     else:
                         zf.write(dirname, ff)
             zf.close()
-            OS.remove(OS.paht.join(str_bag, metadata_file))
+            OS.remove(OS.path.join(str_bag, metadata_file))
 
             LOG.info(f"Uploading {bag_dir} to S3 bucket {folder.bucket}")
             upload.upload_zip_package_to_S3(
                 path_to_zip_package=zipfile, 
-                folder=
+                folder=data_dir_reference,
+                bucket_name=folder.bucket,
+                callback=PRES.UploadProgressConsoleCallback(zipfile),
+                delete_after_upload=True
             )
 
         # Change to directory with data for bagging them
