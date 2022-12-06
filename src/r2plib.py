2ad232b2d3d9b5df57e403a21cc08773303eb54f
Marcelo Garcia <marcelomgarcia@gmail.com>
2022-12-06 11:16:08 +0300

    Save metadata

--- src/r2plib.py
+++ src/r2plib.py
@@ -12,6 +12,26 @@ import os as OS
 path_to_file = TY.Union[str, PL.Path]
 
 
+def save_metadata(bagit_dir: str) -> str:
+    """Save metatada to a file name <bagit_dir>.metadata"""
+
+    title = bagit_dir
+    description = bagit_dir
+
+    metadata_text = """<DeliverableUnit xmlns="http://www.tessella.com/XIP/v4">
+<Title>CATREF</Title>
+<ScopeAndContent>SCOPE</ScopeAndContent>
+</DeliverableUnit>"""
+    metadata_text = metadata_text.replace("CATREF", title)
+    metadata_text = metadata_text.replace("SCOPE", description)
+    metadata_file = f"{bagit_dir}.metadata"
+    metadata_path = OS.path.join(bagit_dir, metadata_file)
+    with open(metadata_path, encoding="utf-8", mode="wt") as fmeta:
+        fmeta.write(metadata_text)
+
+    return metadata_path
+
+
 def add_comment(dirname, filename):
     file = OS.path.join(dirname, filename)
     with open(file, encoding="utf-8", mode="rt") as fd:
@@ -20,6 +40,7 @@ def add_comment(dirname, filename):
     with open(file, encoding="utf-8", mode="wt") as fd:
         fd.writelines(lines)
 
+
 def remove_comment(dirname, filename):
     file = OS.path.join(dirname, filename)
     with open(file, encoding="utf-8", mode="rt") as fd:
@@ -27,6 +48,7 @@ def remove_comment(dirname, filename):
         lines[0] = lines[0].replace("#", "", 1)
     with open(file, encoding="utf-8", mode="wt") as fd:
         fd.writelines(lines)
+
 
 def fetch_title(dirname, filename, default):
     t = default
@@ -41,8 +63,10 @@ def fetch_title(dirname, filename, default):
             d = line.replace("DC_description:", "").strip()
     return t, d
 
+
 class Folder(BaseModel):
-    '''configure the folders on the local file system and on Preservica '''
+    """configure the folders on the local file system and on Preservica"""
+
     parent_folder_id: str
     data_folder: str
     bucket: str
@@ -51,28 +75,28 @@ class Folder(BaseModel):
 
 
 def read_config(input_file: str) -> Folder:
-    '''Read input file and return the folders (path) to ingest into Preservica'''
+    """Read input file and return the folders (path) to ingest into Preservica"""
     config = CONF.ConfigParser()
     config._interpolation = CONF.ExtendedInterpolation()
     config.read(input_file)
 
-    folders = config['FOLDERS']
+    folders = config["FOLDERS"]
     folder = Folder(
-        parent_folder_id = folders.get('parent_folder'), 
-        data_folder = folders.get('data_folder'),
-        bucket = folders.get('bucket'),
-        max_submissions = folders.getint('max_submissions', 10),
-        security_tag = folders.get('security_tag', 'open')
+        parent_folder_id=folders.get("parent_folder"),
+        data_folder=folders.get("data_folder"),
+        bucket=folders.get("bucket"),
+        max_submissions=folders.getint("max_submissions", 10),
+        security_tag=folders.get("security_tag", "open"),
     )
 
     return folder
- 
+
+
 def get_subdirs(data_dir: str) -> list[path_to_file]:
-    '''Returns a list of subdirectories of the 'data_dir' folder.'''
+    """Returns a list of subdirectories of the 'data_dir' folder."""
 
     root_dir = PL.Path(data_dir)
     subdirs = []
 
     subdirs = [dir for dir in root_dir.iterdir() if dir.is_dir()]
     return subdirs
-
